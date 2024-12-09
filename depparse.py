59a363cfcb376c5c9b0ea25bb8d51def9d457e8b
Punyanuch Maitreenukul <107574203+PunY19@users.noreply.github.com>
2024-12-09 23:51:27 +0700

    Update depparse.py

--- depparse.py
+++ depparse.py
@@ -2,6 +2,7 @@ from .load_model import load_model
 import warnings
 
 warnings.filterwarnings("ignore")
+warnings.filterwarnings("ignore", category=UserWarning, module="stanza")
 nlp = load_model()
 
 def depparse(text):
